528d76f7d55405543baae00c87eccf741247567c
anusha <anusha-shrestha@outlook.com>
2024-08-08 20:25:30 +0545

    initial message

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# Create a Flask instance
+app = Flask(__name__)
+
+# Add database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SECRET_KEY'] = "sweetie-pie"
+
+# Initialize database
+db = SQLAlchemy(app)
+
+# Create model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+# Create a form class
+class Form(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+# Create routes
+@app.route('/')
+def index():
+    cras = ["abc", "def"]
+    return render_template('index.html', cras=cras)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', user_name=name)
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = Form()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form submitted successfully!")
+    return render_template("name.html", name=name, form=form)
+
+# Error handlers
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+document.getElementById('demo').innerHTML= "This message was overwritten by js";
+
+// loop in js
+//  // Select all elements with the class 'demo'
+// let elements = document.getElementsByClassName('demo');
+
+// // Loop through each element and update its content
+// for (let i = 0; i < elements.length; i++) {
+//     elements[i].innerHTML = `${i}. message!!`;
+// }
